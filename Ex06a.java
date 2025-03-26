0bd80ff36c00a51648836f9c01d8c0abf47c4cc2
rianaugusto128 <rian.augusto1a@gmail.com>
2025-03-26 13:49:16 -0300

    Programa para identificar se um número é impar ou par

---
+++ Ex06a.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+Faça um algoritmo que receba um número
+inteiro qualquer e mostre se ele é par ou impar.
+
+*******************************************************************************/
+
+import java.util.Scanner;
+public class Ex06{ 
+
+    public static void main (String [] args){
+        double n1, resultado;
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("escolha um número: ");
+        n1 = ler.nextDouble();
+        
+        resultado = n1 % 2;
+ 
+     
+        if(resultado == 1)
+          System.out.println("impar");
+          
+        else 
+          System.out.println("par");
+    }
+}
+	 
+	   
+	  
+	   
+	   
