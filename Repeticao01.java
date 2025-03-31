6bc9deb80da7a37174184a19fe9dfb657144560f
rianaugusto128 <rian.augusto1a@gmail.com>
2025-03-31 11:46:07 -0300

    Add files via upload
    exercicio repetição

---
+++ Repeticao01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Repeticao01{
+    public static void main (String [] args){
+        Scanner ler = new Scanner (System.in);
+        int n;
+        
+        System.out.print("Digite um númerp maior que zero: ");
+        n = ler.nextInt();
+        
+        
+        for (int i = 0; i <= n; i++)
+            System.out.println("Número "+i);
+    }
+}
