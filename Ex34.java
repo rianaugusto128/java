9205f0726664218213880a9e780bcea15e60c9b2
rianaugusto128 <rian.augusto1a@gmail.com>
2025-04-09 08:06:47 -0300

    exemplo

---
+++ Ex34.java
@@ -0,0 +1,10 @@
+public class Ex34 {
+    public static void main(String[] args){
+        int contador = 0;
+        
+        do{
+            System.out.println(contador);
+            contador++;
+        } while (contador <= 5);
+    }
+}
