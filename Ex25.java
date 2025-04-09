e2dd8df1645dc8b980bfa986f6dbc2bb02574160
rianaugusto128 <rian.augusto1a@gmail.com>
2025-04-09 07:56:40 -0300

    programa utilizando while, número de 0 a 100

---
+++ Ex25.java
@@ -0,0 +1,18 @@
+/*
+
+Faça um programa, utilizando while, que mostre na tela os números de 0 a 100.
+
+*/
+
+
+public class Ex25{
+    public static void main(String [] args){
+        int numero = 0;
+        
+        while (numero <= 100){
+           System.out.println(numero);
+           numero++;
+           
+        }
+    }
+}
