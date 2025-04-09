20447d7dab941c420b94bd4ebf0fbc2129145403
rianaugusto128 <rian.augusto1a@gmail.com>
2025-04-09 07:39:36 -0300

    um programa que exiba 10 vezes uma mensagem

---
+++ Ex14.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Ex14{
+    public static void main (String [] args){
+        Scanner ler = new Scanner (System.in);
+        int n;
+        
+        System.out.print("Quantas vezes você gosta de estudar: ");
+        n = ler.nextInt();
+        
+        for (int i = 1; i <= 10; i++){
+
+           System.out.println("Eu gosto de estudar algoritimos"); 
+        }
+    }
+}
